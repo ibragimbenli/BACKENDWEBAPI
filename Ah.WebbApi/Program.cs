9118c63a3f4a5c034650b3150af7ca47a7701235
ibrahim.benli <ibrahim.benli@geos.com.tr>
2023-07-23 14:41:39 +0300

    CircleIgnore iç içe 32 derinlikli döngü engellendi

--- Ah.WebbApi/Program.cs
+++ Ah.WebbApi/Program.cs
@@ -2,6 +2,11 @@ using Ah.Business.Implementation;
 using Ah.Business.Interface;
 using Ah.DataAccess.EF.Repositoryies;
 using Ah.DataAccess.Interfaces;
+using System.Net.Http.Json;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +23,16 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeBs, EmployeeBs>();
 
+builder.Services.AddControllersWithViews().AddJsonOptions(x =>
+x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+//var json = JsonConvert.SerializeObject(harry, Formatting.Indented,
+//                    new JsonSerializerSettings()
+//                    {
+//                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+//                    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,3 +47,15 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+public class MyTransparentJsonNamingPolicy : JsonNamingPolicy
+{
+    // You can came up any custom transformation here, so instead just transparently
+    // pass through the original C# class property name, it is possible to explicit
+    // convert to PascalCase, etc:
+    public override string ConvertName(string name)
+    {
+        var camelCase = name.Substring(0, 1).ToLowerInvariant();
+        camelCase = camelCase + name.Substring(1, name.Length - 1);
+        return camelCase;
+    }
+}
