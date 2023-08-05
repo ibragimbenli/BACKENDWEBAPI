d0393ae27549de6b61d416eefad4940124ae3c24
ibrahim.benli <ibrahim.benli@geos.com.tr>
2023-08-05 14:44:30 +0300

    MapperProfile'da PrdocutGetDto updated

--- Ah.WebbApi/Profiles/MapperProfile.cs
+++ Ah.WebbApi/Profiles/MapperProfile.cs
@@ -8,7 +8,7 @@ namespace Ah.WebbApi.Profiles
     {
         public MapperProfile()
         {
-            CreateMap<Product, ProductDto>().ForMember(dst => dst.CategoryName, X => X.MapFrom(src => src.Category.CategoryName));
+            CreateMap<Product, ProductGetDto>().ForMember(dst => dst.CategoryName, X => X.MapFrom(src => src.Category.CategoryName));
 
             //CreateMap<Product, ProductDto>().ForMember(hedef => hedef.CategoryName, kaynak => kaynak.MapFrom(src => src.Category.CategoryName));
         }
