c49642fca7213838ef3835984605cd0ba6d36e63
ibrahim.benli <ibrahim.benli@geos.com.tr>
2023-08-05 18:10:53 +0300

    ProductPost ve Put Dto classları oluşturuldu

---
+++ Ah.Model/Dtos/Product/ProductPostDto.cs
@@ -0,0 +1,18 @@
+﻿using CommonTypesLayer.Model;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ah.Model.Dtos.Product
+{
+    public class ProductPostDto : IDto
+    {
+        public string? ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Ah.Model/Dtos/Product/ProductPutDto.cs
@@ -0,0 +1,18 @@
+﻿using CommonTypesLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ah.Model.Dtos.Product
+{
+    public class ProductPutDto : IDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
